abd310601766c7318d8903055dd088e9e0128716
UniP62 <baptiste.famchon@sfr.fr>
2018-02-16 14:09:43 +0100

    feat() ajout de la fonction et page new

--- lib/chien_web/controllers/user_controller.ex
+++ lib/chien_web/controllers/user_controller.ex
@@ -12,5 +12,10 @@ defmodule ChienWeb.UserController do
       user = Repo.get!(User, id)
       render(conn, "show.html", user: user)
     end
+    
+    def new(conn, _params) do
+      changeset = User.changeset(%User{}, %{})
+      render(conn, "new.html", changeset: changeset)
+    end
 
 end
