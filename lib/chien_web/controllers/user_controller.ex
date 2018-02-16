0ea2fb4c21d4cdcbd61cb74d6636a38b647d4c15
UniP62 <baptiste.famchon@sfr.fr>
2018-02-16 08:16:06 +0100

    feat(): ajout du show sur un utilisateur

--- lib/chien_web/controllers/user_controller.ex
+++ lib/chien_web/controllers/user_controller.ex
@@ -8,4 +8,9 @@ defmodule ChienWeb.UserController do
       render(conn, "index.html", users: users)
     end
 
+    def show(conn, %{"id" => id}) do
+      user = Repo.get!(User, id)
+      render(conn, "show.html", user: user)
+    end
+
 end
