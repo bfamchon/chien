064a5607269af3b7116f8199d2fefbc7d2a78faf
UniP62 <baptiste.famchon@sfr.fr>
2018-02-26 12:50:30 +0100

    feat(user): ajout des methodes edit update et delete pour laffichage des pages et la gestion des fonctionnalites

--- lib/chien_web/controllers/user_controller.ex
+++ lib/chien_web/controllers/user_controller.ex
@@ -18,4 +18,46 @@ defmodule ChienWeb.UserController do
       render(conn, "new.html", changeset: changeset)
     end
 
+    def create(conn, %{"user" => user_params}) do
+      changeset = User.changeset(%User{}, user_params)
+
+      case Repo.insert(changeset) do
+        {:ok, _user} ->
+          conn
+          |> put_flash(:info, "User created successfully BG !")
+          |> redirect(to: user_path(conn, :index))
+        {:error, changeset} ->
+          render(conn, "new.html", changeset: changeset)
+        end
+    end
+
+    def edit(conn, %{"id" => id}) do
+      user = Repo.get!(User, id)
+      changeset = User.changeset(user, %{})
+      render(conn, "edit.html", user: user, changeset: changeset)
+    end
+
+    def update(conn, %{"id" => id, "user" => user_params}) do
+      user = Repo.get!(User, id)
+      changeset = User.changeset(user, user_params)
+
+      case Repo.update(changeset) do
+        {:ok, user} ->
+          conn
+          |> put_flash(:info, "User updated successfully BG !")
+          |> redirect(to: user_path(conn, :show, user))
+        {:error, changeset} ->
+          render(conn, "edit.html", user: user, changeset: changeset)
+        end
+    end
+
+    def delete(conn, %{"id" => id}) do
+      user = Repo.get!(User, id)
+      Repo.delete!(user)
+
+      conn
+      |> put_flash(:warning, "User udeleted successfully BG !")
+      |> redirect(to: user_path(conn, :index))   
+    end
+
 end
